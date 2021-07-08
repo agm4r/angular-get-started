8a5c36b21ddcf6762f77424ce5c843b2b9d7500a
agm4r <agmarputra@gmail.com>
2021-07-08 09:24:35 +0700

    add getShippingPrices() method in cart.servise.ts

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -18,4 +18,12 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{ type: String; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+
+  constructor(private http: HttpClient) {}
 }
